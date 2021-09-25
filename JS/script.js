abb20322a3ee32aebcc11ff510d1cf6905eefcb3
Nitish <tnitish654@gmail.com>
2021-09-25 15:09:47 +0800

    Ignore In-text Citation works now

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,41 @@
 const wordInput = document.getElementById("wordInput")
 const wordCounter = document.getElementById("wordCounterText")
 const charCounter = document.getElementById("charCounterText")
+const cb = document.getElementById("inTextCitationIgnore")
 
 let wordInputText
+let cbState
 
 wordInput.addEventListener("input", count)
+
+cb.addEventListener("change", () => {
+	ignoreCiteState(wordInputText)
+	countWords(wordInputText)
+	countChar(wordInputText)
+})
 
 function count() {
 	wordInputText = wordInput.value
 
+	ignoreCiteState(wordInputText)
 	countWords(wordInputText)
 	countChar(wordInputText)
 }
+
+function ignoreCiteState(str) {
+	console.log(cbState)
+	cbState = cb.checked
+	
+	if (cbState) {
+		wordInputText = str.replace(/ *\([^)]*\) */g, "")
+	} else (
+		wordInputText = wordInput.value
+	)
+
+	return wordInputText
+}
+
+
 
 function countWords(str) {
 	let wordNum = str.split(" ").length
